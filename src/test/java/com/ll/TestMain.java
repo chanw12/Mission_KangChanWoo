b3dc43881202e3a31d709cf2c10ed351fd7a56ac
chanw12 <wooju9812127@gmail.com>
2023-11-06 09:30:05 +0900

    명령어 입력창 구현 테스트 생성

---
+++ src/test/java/com/ll/TestMain.java
@@ -0,0 +1,21 @@
+package com.ll;
+
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayOutputStream;
+
+public class TestMain {
+    @Test
+    @DisplayName("명령어 입력창 구현")
+    void t1(){
+        ByteArrayOutputStream byteArrayOutputStream = TestUtil.setOutToByteArray();
+
+
+        System.out.print("명령)");
+        String out = byteArrayOutputStream.toString().trim();
+        Assertions.assertThat(out).isEqualTo("명령)");
+    }
+}
