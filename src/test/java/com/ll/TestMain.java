1c4c8474388dc8b353753f73b0dc869581937176
chanw12 <wooju9812127@gmail.com>
2023-11-06 09:34:01 +0900

    명령어 입력 확인 테스트 생성

--- src/test/java/com/ll/TestMain.java
+++ src/test/java/com/ll/TestMain.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.io.ByteArrayOutputStream;
+import java.util.Scanner;
 
 public class TestMain {
     @Test
@@ -17,5 +18,17 @@ public class TestMain {
         System.out.print("명령)");
         String out = byteArrayOutputStream.toString().trim();
         Assertions.assertThat(out).isEqualTo("명령)");
+    }
+
+    @Test
+    @DisplayName("명령어 입력 확인")
+    void t2(){
+        ByteArrayOutputStream byteArrayOutputStream = TestUtil.setOutToByteArray();
+        System.out.print("명령)");
+        String out = byteArrayOutputStream.toString().trim();
+        Scanner scanner = TestUtil.genScanner("종료");
+        String cmd = scanner.nextLine();
+
+        Assertions.assertThat(cmd).isEqualTo("종료");
     }
 }
