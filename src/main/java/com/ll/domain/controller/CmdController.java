fa3545db79a32a3b9d57cd09cc2f406b31f3a612
chanw12 <wooju9812127@gmail.com>
2023-11-06 11:51:01 +0900

    삭제기능구현

--- src/main/java/com/ll/domain/controller/CmdController.java
+++ src/main/java/com/ll/domain/controller/CmdController.java
@@ -3,6 +3,7 @@ package com.ll.domain.controller;
 import com.ll.domain.Context;
 import com.ll.domain.WiseSaying;
 import com.ll.domain.repository.WiseSayingRepo;
+import com.ll.domain.utill.Rq;
 
 import java.util.Scanner;
 
@@ -19,8 +20,8 @@ public class CmdController {
         while (true) {
             System.out.print("명령) ");
             String cmd = scanner.nextLine();
-
-            if (cmd.equals("종료")) {
+            Rq rq = new Rq(cmd);
+            if (rq.getAction().equals("종료")) {
                 break;
             } else if (cmd.equals("등록")) {
 
@@ -31,13 +32,25 @@ public class CmdController {
                 WiseSaying wiseSaying = new WiseSaying(WiseSaying.idVal++, content, authorName);
                 System.out.println(wiseSaying.getId()+ "번 명언이 등록되었습니다.");
                 wiseSayingRepo.getWiseSayingList().add(wiseSaying);
-            }else if(cmd.equals("목록")){
+            }else if(rq.getAction().equals("목록")){
                 System.out.println("번호 / 작가 / 명언");
                 System.out.println("----------------------");
                 for (WiseSaying ws: wiseSayingRepo.getWiseSayingList()){
                     System.out.println(ws.getId()+ " / "+ ws.getAuthor()+ " / "+ ws.getBody());
                 }
+            }else if(rq.getAction().equals("삭제")){
+                int id = rq.getParamAsInt("id",0);
+                if(wiseSayingRepo.getWiseSayingList().stream().anyMatch(ws -> ws.getId() == id)){
+
+                    WiseSaying wiseSaying = wiseSayingRepo.getWiseSayingList().stream().filter(ws -> ws.getId() == id).findFirst().get();
+                    System.out.println(id+"번 명언이 삭제되었습니다.");
+                    wiseSayingRepo.getWiseSayingList().remove(wiseSaying);
+                }else{
+                    System.out.println(id+"번 명언은 존재하지 않습니다.");
+                }
             }
+
+
         }
     }
 }
