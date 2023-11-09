307f6e22fb9469a2274e54c501697ca73bbba295
chanw12 <wooju9812127@gmail.com>
2023-11-09 16:28:07 +0900

    ..

--- src/main/java/com/ll/domain/controller/BoardController.java
+++ src/main/java/com/ll/domain/controller/BoardController.java
@@ -1,12 +1,9 @@
 package com.ll.domain.controller;
 
-import com.ll.domain.SimpleDb;
-import com.ll.domain.Sql;
 import com.ll.domain.WiseSaying;
 import com.ll.domain.repository.WiseSayingRepo;
 import lombok.RequiredArgsConstructor;
 
-import java.util.List;
 import java.util.Scanner;
 
 @RequiredArgsConstructor
@@ -22,23 +19,12 @@ public class BoardController {
         String authorName = scanner.nextLine();
         WiseSaying wiseSaying = new WiseSaying(WiseSaying.idVal++, content, authorName);
         System.out.println(wiseSaying.getId()+ "번 명언이 등록되었습니다.");
-        Sql sql = SimpleDb.genSql();
-        sql.append("INSERT INTO Wisesaying (body, author)")
-                .append( "VALUES (?, ?)",content,authorName);
-        sql.insert();
-
-
-//        wiseSayingRepo.getWiseSayingList().add(wiseSaying);
+        wiseSayingRepo.registWiseSaying(wiseSaying);
     }
     public void list(){
         System.out.println("번호 / 작가 / 명언");
         System.out.println("----------------------");
-        Sql sql = SimpleDb.genSql();
-        sql.append("select * from Wisesaying");
-        List<WiseSaying> wiseSayingList = sql.selectRows(WiseSaying.class);
-        wiseSayingList.stream().forEach(i->{
-            System.out.println(i.getId() +" / "+i.getAuthor()+ " / "+i.getBody());
-        });
+        wiseSayingRepo.listingWiseSaying();
     }
     public void delete(long id){
 
@@ -49,8 +35,6 @@ public class BoardController {
         }else{
             System.out.println(id+"번 명언은 존재하지 않습니다.");
         }
-
-
     }
     public void modi(long id){
 
@@ -60,13 +44,10 @@ public class BoardController {
             System.out.println("명언(기존) : " + wiseSaying.getBody());
             System.out.print("명언 : ");
             String newbody = scanner.nextLine();
-
             System.out.println("작가(기존) : "+ wiseSaying.getAuthor());
             System.out.print("작가: ");
             String newAuthor = scanner.nextLine();
             wiseSayingRepo.modiWiseSaying(id,newbody,newAuthor);
-
-
         }else{
             System.out.println(id+"번 명언은 존재하지 않습니다.");
         }
--- src/main/java/com/ll/domain/repository/WiseSayingRepo.java
+++ src/main/java/com/ll/domain/repository/WiseSayingRepo.java
@@ -55,6 +55,21 @@ public class WiseSayingRepo {
                 .update();
     }
 
+    public void listingWiseSaying(){
+        Sql sql = SimpleDb.genSql();
+        sql.append("select * from Wisesaying");
+        List<WiseSaying> wiseSayingList = sql.selectRows(WiseSaying.class);
+        wiseSayingList.stream().forEach(i->{
+            System.out.println(i.getId() +" / "+i.getAuthor()+ " / "+i.getBody());
+        });
+    }
+    public void registWiseSaying(WiseSaying wiseSaying){
+        Sql sql = SimpleDb.genSql();
+        sql.append("INSERT INTO Wisesaying (body, author)")
+                .append( "VALUES (?, ?)",wiseSaying.getBody(),wiseSaying.getAuthor());
+        sql.insert();
+    }
+
 
     public static void setFilename(String filename) {
         WiseSayingRepo.filename = filename;
@@ -67,64 +82,5 @@ public class WiseSayingRepo {
     public WiseSayingRepo(){
         wiseSayingList = new ArrayList<>();
     }
-//    public static void setWiseSayingList(List<WiseSaying> wiseSayingList) {
-//        WiseSayingRepo.wiseSayingList = wiseSayingList;
-//    }
-//
-//    public static void saveData(List<WiseSaying> data) {
-//        try (ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(filename))) {
-//            oos.writeObject(data);
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
-//    }
 
-//    public static List<WiseSaying> loadData() {
-//        List<WiseSaying> data = new ArrayList<>();
-//        try (ObjectInputStream ois = new ObjectInputStream(new FileInputStream(filename))) {
-//            data = (List<WiseSaying>) ois.readObject();
-//        }catch (FileNotFoundException e) {
-//            // 파일이 존재하지 않으면 새로운 파일 생성
-//            try {
-//                File file = new File("data.txt");
-//                file.createNewFile();
-//            } catch (IOException ex) {
-//                ex.printStackTrace();
-//            }
-//        } catch (IOException | ClassNotFoundException e) {
-//            e.printStackTrace();
-//            return new ArrayList<>();
-//        }
-//        OptionalInt maxId = wiseSayingList.stream().mapToInt(fs -> fs.getId()).max();
-//        if (maxId.isPresent()) {
-//            WiseSaying.idVal = maxId.getAsInt() + 1;
-//        }
-//        return data;
-//    }
-
-
-
-//    static ObjectMapper mapper = new ObjectMapper();
-//    public static void FileLoad(){
-//
-//        try {
-//            WiseSayingRepo.setWiseSayingList(new ArrayList<>(Arrays.asList(mapper.readValue(new File(filename), WiseSaying[].class))));
-//
-//            OptionalInt maxId = wiseSayingList.stream().mapToInt(fs -> fs.getId()).max();
-//            if (maxId.isPresent()) {
-//                WiseSaying.idVal = maxId.getAsInt() + 1;
-//            }
-//
-//        } catch (IOException ex) {
-//            System.out.println("파일 읽기 오류: " + ex.getMessage());
-//        }
-//    }
-
-//    public static void FileSave(List<WiseSaying> wiseSayingList){
-//        try {
-//            mapper.writeValue(new File(filename), wiseSayingList);
-//        } catch (IOException ex) {
-//            System.out.println("파일 쓰기 오류" + ex.getMessage());
-//        }
-//    }
 }
